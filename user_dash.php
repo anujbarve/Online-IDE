d0b3073ebb9cf4627494cf88db58962304636216
anujbarve <anujbarve27@gmail.com>
2022-04-21 15:26:12 +0530

    UI fixes

--- user_dash.php
+++ user_dash.php
@@ -20,6 +20,10 @@
 .files{
   background-color: blue;
 } */
+
+.profile-photo{
+  border-radius: 50%;
+}
 </style>
 
 
@@ -27,22 +31,22 @@
 
 <div class="m-5 main flex md:flex-row flex-col">
 <div class="profile w-full md:w-1/3">
-
-01
-
 <!-- Profile -->
-
-<div class="flex justify-center">
-  <div class="block p-6 rounded-lg shadow-lg bg-white max-w-sm">
-    <h5 class="text-gray-900 text-xl leading-tight font-medium mb-2">Card title</h5>
-    <p class="text-gray-700 text-base mb-4">
-      Some quick example text to build on the card title and make up the bulk of the card's
-      content.
-    </p>
-    <button type="button" class=" inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">Button</button>
+<div class="profile flex justify-center">
+  <div class="rounded-lg shadow-lg bg-cyan-200 max-w-sm">
+    <a class="m-10 flex justify-center" data-mdb-ripple="true" data-mdb-ripple-color="light">
+      <img class="profile-photo" width="200px" src="https://angularjs-template.herokuapp.com/sing-app/dist/demo/img/people/a5.jpg" alt=""/>
+    </a>
+    <div class="p-6">
+      <h5 class="text-gray-900 text-xl font-medium mb-2">Card title</h5>
+      <p class="text-gray-700 text-base mb-4">
+        Some quick example text to build on the card title and make up the bulk of the card's
+        content.
+      </p>
+      <button type="button" class=" inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">Button</button>
+    </div>
   </div>
 </div>
-
 <!-- /Profile -->
 
 
@@ -51,66 +55,40 @@
 
 <div class="files w-full md:w-2/3" >
 
-02
-
 <!-- Files -->
 
-<div class="flex flex-col">
-  <div class="overflow-x-auto sm:-mx-6 lg:-mx-8">
-    <div class="py-2 inline-block min-w-full sm:px-6 lg:px-8">
+<div class="flex flex-col ">
+  <div class="overflow-x-auto ">
+    <div class="inline-block min-w-full sm:px-6 lg:px-8">
       <div class="overflow-hidden">
-        <table class="min-w-full">
+        <table class="w-full table-fixed bg-slate-300">
           <thead class="border-b">
             <tr>
-              <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 text-left">
-                #
+              <th scope="col" style="width: 10%;" class="text-sm font-medium text-gray-900 px-6 py-4 text-left">
+                Icon
               </th>
-              <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 text-left">
-                First
-              </th>
-              <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 text-left">
-                Last
-              </th>
-              <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 text-left">
-                Handle
+              <th scope="col" style="width: 90%;" class="text-sm font-medium text-gray-900 px-6 py-4 text-left">
+                Files
               </th>
             </tr>
           </thead>
           <tbody>
-            <tr class="border-b">
+            <tr class="border-b ">
               <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">1</td>
               <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
                 Mark
               </td>
-              <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                Otto
-              </td>
-              <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                @mdo
-              </td>
             </tr>
-            <tr class="bg-white border-b">
+            <tr class="border-b">
               <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">2</td>
               <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
                 Jacob
               </td>
-              <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                Thornton
-              </td>
-              <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                @fat
-              </td>
             </tr>
-            <tr class="bg-white border-b">
+            <tr class="border-b">
               <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">3</td>
               <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
                 Larry
-              </td>
-              <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                Wild
-              </td>
-              <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                @twitter
               </td>
             </tr>
           </tbody>
